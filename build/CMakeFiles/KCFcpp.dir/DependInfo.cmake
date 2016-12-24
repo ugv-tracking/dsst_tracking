82baa719885085073fd8b4ef938ce07bae2eea3c
Rohohertz <hitmaxtom@gmail.com>
2016-12-24 21:33:02 +0800

    ok

---
+++ build/CMakeFiles/KCFcpp.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/3rdparty/cv_ext/init_box_selector.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/init_box_selector.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/3rdparty/cv_ext/math_spectrums.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/math_spectrums.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/3rdparty/cv_ext/shift.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/shift.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/3rdparty/cv_ext/tracker_run.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/tracker_run.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/3rdparty/piotr/src/gradientMex.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/3rdparty/piotr/src/gradientMex.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/cf_libs/common/math_helper.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/cf_libs/common/math_helper.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/main/image_acquisition.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/main/image_acquisition.cpp.o"
+  "/home/i/code_base/ACC_CAR/dsst_tracking/src/main/main_kcf.cpp" "/home/i/code_base/ACC_CAR/dsst_tracking/build/CMakeFiles/KCFcpp.dir/src/main/main_kcf.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/share/OpenCV/include"
+  "../src/3rdparty/tclap"
+  "../src/3rdparty/cv_ext"
+  "../src/3rdparty/piotr"
+  "../src/3rdparty/piotr/src"
+  "../src/main"
+  "../src/cf_libs/kcf"
+  "../src/cf_libs/dsst"
+  "../src/cf_libs/common"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/KCFcpp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/KCFcpp.dir/src/main/main_kcf.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/main/image_acquisition.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/init_box_selector.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/tracker_run.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/cf_libs/common/math_helper.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/shift.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/3rdparty/cv_ext/math_spectrums.cpp.o"
+  "CMakeFiles/KCFcpp.dir/src/3rdparty/piotr/src/gradientMex.cpp.o"
+  "KCFcpp.pdb"
+  "KCFcpp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/KCFcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
