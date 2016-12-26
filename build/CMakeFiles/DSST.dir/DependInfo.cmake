491fd3b10ae11e9c2b76c9efa4612706a075eb81
maxtom <hitmaxtom@gmail.com>
2016-12-26 09:47:07 +0800

    ok

---
+++ build/CMakeFiles/DSST.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/i/code_base/dsst_tracking/src/3rdparty/cv_ext/init_box_selector.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/3rdparty/cv_ext/init_box_selector.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/3rdparty/cv_ext/math_spectrums.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/3rdparty/cv_ext/math_spectrums.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/3rdparty/cv_ext/shift.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/3rdparty/cv_ext/shift.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/3rdparty/cv_ext/tracker_run.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/3rdparty/cv_ext/tracker_run.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/3rdparty/piotr/src/gradientMex.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/3rdparty/piotr/src/gradientMex.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/cf_libs/common/math_helper.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/cf_libs/common/math_helper.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/main/image_acquisition.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/main/image_acquisition.cpp.o"
+  "/home/i/code_base/dsst_tracking/src/main/main_dsst.cpp" "/home/i/code_base/dsst_tracking/build/CMakeFiles/DSST.dir/src/main/main_dsst.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/python2.7"
+  "/usr/include/x86_64-linux-gnu/python2.7"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  ".."
+  "../src"
+  "../src/cf_libs/kcf"
+  "../src/cf_libs/dsst"
+  "../src/cf_libs/common"
+  "../pybind11/include"
+  "/usr/local/share/OpenCV/include"
+  "../src/3rdparty/tclap"
+  "../src/3rdparty/cv_ext"
+  "../src/3rdparty/piotr"
+  "../src/3rdparty/piotr/src"
+  "../src/main"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
